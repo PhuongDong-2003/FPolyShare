11b86fdba1b3ac48e0d76c425704fc3567f1b322
PhuongDong-2003 <nguyenphuongdong4102003@gmail.com>
2023-11-12 22:40:20 +0700

    update entity

--- API/src/main/java/com/example/api/Entity/FeedBack.java
+++ API/src/main/java/com/example/api/Entity/FeedBack.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @Table(name = "Feedback")
 public class FeedBack {
-//update
+//update entity
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id")
