3b94919241648e21f434e974534a43eaa3eb228a
Nhut Anh <nhutanh1605@gmail.com>
2023-11-14 21:18:49 +0700

    update create user

--- API/src/main/java/com/example/api/Repository/UserRepository.java
+++ API/src/main/java/com/example/api/Repository/UserRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    boolean existsByMssv(String mssv);
+    boolean existsByUsername(String username);
 }
